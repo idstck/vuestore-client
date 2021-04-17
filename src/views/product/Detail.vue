a1669c96a4121c448666e1844c6538bdf0e03180
lunadiotic <ahmadhakim404@gmail.com>
2021-04-17 14:33:03 +0700

    menampilkan data berdasarakan id

--- src/views/product/Detail.vue
+++ src/views/product/Detail.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
-      <h1>Detail Product</h1>
+      <div id="page-wrap">
+        <div id="img-wrap">
+          <img :src="product.imageUrl" alt="">
+        </div>
+        <div id="product-details">
+          <h1>{{ product.name }}</h1>
+          <h3 id="price">Rp{{ product.price }}</h3>
+          <p>Average rating: {{ product.averageRating }}</p>
+          <button id="add-to-cart">Add to Cart</button>
+          <p>{{ product.description }}</p>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import { products } from '../../data-seed'
 
+export default {
+  data() {
+    return {
+      products
+    }
+  },
+  computed: {
+    product() {
+      return this.products.find((p) => {
+        return p.id === this.$route.params.id
+      })
+    }
+  },
+  mounted() {
+    console.log(this.product)
+  }
 }
 </script>
 
@@ -18,7 +45,7 @@ export default {
   }
 
   #img-wrap {
-    text-align: center;
+    text-align: center;;
   }
 
   img {
