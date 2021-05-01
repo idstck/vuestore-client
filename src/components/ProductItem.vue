9b64d7d09716b664c09bffa32e4e624731af6a45
Lunadiotic <ahmadhakim404@gmail.com>
2021-05-01 12:50:18 +0700

    menampilkan detail produk dari api

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -3,7 +3,7 @@
         <img :src="`http://localhost:8000${product.imageUrl}`" alt="">
         <h3 class="product-name">{{ product.name }}</h3>
         <p class="product-price">Rp{{ product.price }}</p>
-        <router-link :to="{ name: 'product-detail', params: {id: product.id} }">
+        <router-link :to="{ name: 'product-detail', params: {id: product.code} }">
             <button>Detail</button>
         </router-link>
     </div>
--- src/views/product/Detail.vue
+++ src/views/product/Detail.vue
@@ -2,7 +2,7 @@
   <div>
       <div id="page-wrap" v-if="product">
         <div id="img-wrap">
-          <img :src="product.imageUrl" alt="">
+          <img :src="`http://localhost:8000${product.imageUrl}`" alt="">
         </div>
         <div id="product-details">
           <h1>{{ product.name }}</h1>
@@ -18,7 +18,8 @@
 </template>
 
 <script>
-import { products } from '../../data-seed'
+import axios from 'axios'
+// import { products } from '../../data-seed'
 import NotFound from '../errors/404'
 
 export default {
@@ -27,18 +28,13 @@ export default {
   },
   data() {
     return {
-      products
+      product: {}
     }
   },
-  computed: {
-    product() {
-      return this.products.find((p) => {
-        return p.id === this.$route.params.id
-      })
-    }
-  },
-  mounted() {
-    console.log(this.product)
+  async created() {
+    const code = this.$route.params.id
+    const result = await axios.get(`http://localhost:8000/api/products/${code}`)
+    this.product = result.data
   }
 }
 </script>
